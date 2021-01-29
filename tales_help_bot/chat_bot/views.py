276e9a9e2eaa06f06a8c9413649434de114555bb
hyeongjic <khj3753@gmail.com>
2021-01-30 01:28:40 +0900

    wait moment later...?

--- tales_help_bot/chat_bot/views.py
+++ tales_help_bot/chat_bot/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("charmant")
+
+
+def keyboard(request):
+    return JsonResponse({
+        'type': 'buttons',
+        'buttons': ['오늘','내일']
+    })
+
+@csrf_exempt
+def answer(request):
+    json_str = ((request.body).decode('utf-8'))
+    received_json_data = json.loads(json_str)
+    data_content = received_json_data['content']
+
+    if data_content == '오늘':
+        today = "오늘에 대한 답"
+        return JsonResponse({
+            'message': {
+                'text': today
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['모레', '내일']
+            }
+        })
+    elif data_content == '내일':
+        tomorrow = "내일에 대한 해답"
+        return JsonResponse({
+            'message': {
+                'text': tomorrow
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['오늘', '모레']
+            }
+        })
+    elif data_content == '모레':
+        after_tomorrow = '모래성모래성'
+        return JsonResponse({
+            'message': {
+                'text': after_tomorrow
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['오늘', '내일']
+            }
+        })
